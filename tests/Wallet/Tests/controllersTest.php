60388cd423f28996992fe98884911dde5bc086db
duxor <dullle@live.com>
2017-06-20 21:29:29 +0200

    Init 1.0.1 - PHPUnit

---
+++ tests/Wallet/Tests/controllersTest.php
@@ -0,0 +1,27 @@
+<?php
+namespace Wallet\Tests;
+
+use Silex\WebTestCase;
+
+class controllersTest extends WebTestCase
+{
+    public function testGetHomepage()
+    {
+        $client = $this->createClient();
+        $client->followRedirects(true);
+        $crawler = $client->request('GET', '/');
+
+        $this->assertTrue($client->getResponse()->isOk());
+        $this->assertContains('Wallet with Silex Framework!', $crawler->filter('body')->text());
+    }
+
+    public function createApplication()
+    {
+        $app = require __DIR__ . '/../../../src/app.php';
+        require __DIR__ . '/../../../config/dev.php';
+        require __DIR__ . '/../../../src/controllers.php';
+        $app['session.test'] = true;
+
+        return $this->app = $app;
+    }
+}
